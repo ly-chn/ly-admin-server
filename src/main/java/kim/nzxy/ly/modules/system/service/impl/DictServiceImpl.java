8cd5ab728a60f7adf2545b1630da9d730f6ed6fd
xy <3293232930@qq.com>
2023-01-12 17:29:13 +0800

    update to springboot3.1

--- src/main/java/kim/nzxy/ly/modules/system/service/impl/DictServiceImpl.java
+++ src/main/java/kim/nzxy/ly/modules/system/service/impl/DictServiceImpl.java
@@ -1,19 +1,12 @@
 package kim.nzxy.ly.modules.system.service.impl;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import kim.nzxy.ly.modules.system.entity.Dict;
 import kim.nzxy.ly.modules.system.mapper.DictMapper;
 import kim.nzxy.ly.modules.system.service.DictService;
+import org.springframework.stereotype.Service;
 
 @Service
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
 
 }
-
-
-
-
-
